d2f06115b6c9340387628744bb2b0b2fe4f0d3c9
TarangGarlapally <taranggarlapally@gmail.com>
2021-09-10 22:55:35 +0530

    configured firebase

--- server.js
+++ server.js
@@ -1,49 +1,37 @@
-const http=require('http')
-const fs=require('fs')
-const express= require('express')
-var app=express()
-const port=3000
-const students= require('./student')
+const http = require('http')
+const fs = require('fs')
+const express = require('express')
+const firebase = require('./firebase')
+
+var app = express()
+const port = 3000
 app.use(express.json())
-/*** 
-const server=http.createServer(function(req,res){
 
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+app.listen(port, () => {
+    console.log('server is listening on port ' + port)
 })
 
-server.listen(port,function(error){
-    if(error){
-        console.log('something went wrong',error)
-    }
-    else{
-        console.log('server is listening at port '+ port)
-    }
-})
-***/
 
-app.listen(port,()=>{
-    console.log('server is listening on port '+ port)
-})
 
-app.get('/',(req,res)=>{
-    //res.send('Hello world')
-    res.json({message:'Hello world..'})
-    
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello world..' })
 })
 
 var M1 = { colour: 'blue' };
 var M2 = { width: 100 };
 
-app.get('/test', function(req,res,next) {
-  res.jsonp( { foo: M1.colour, bar: M2.width } );
+app.get('/test', function (req, res, next) {
+    res.jsonp({ foo: M1.colour, bar: M2.width });
 });
 
-app.post('/',(req,res)=>{
-    const user={
-        id:students.length+1,
-        first_name:req.body.first_name,
-        last_name:req.body.last_name,
-        email: req.body.email
-    }
-    students.push(user)
-    res.json(user)
+app.post('/send-model', (req, res) => {
+    res.json(req.body.email + "abc")
 })
