ac5fc38ae9e0cf6e4a8cdd25fe064d8d15717b31
TarangGarlapally <taranggarlapally@gmail.com>
2021-09-11 21:33:11 +0530

    updated PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const services = require('./services')
 
 var app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
