e108451dfd238cd614cc2f7417818ae7003f351b
TarangGarlapally <taranggarlapally@gmail.com>
2021-09-13 17:03:11 +0530

    before mege

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const express = require('express')
 const services = require('./services')
 const file = require('./fileOp')
 
-var admin = require('firebase-admin');
-const db = admin.firestore();
-
 var app = express()
 const port = process.env.PORT || 3000
 
@@ -31,51 +28,34 @@ app.listen(port, () => {
 
 
 app.get('/', (req, res) => {
-    
+
     res.json({ message: 'Hello world..' })
 })
 
 app.get('/get-best-model', (req, res) => {
     services.removing()
 
-    fs.readFile('localcoef.temporary.txt' ,function (err, data) {
-        if(err) throw err;
-            
-            data1=data.toString()
-            arr=data1.split(',').map(Number)
-            var array=[]
-            array=[arr]
-            myMethod(array)
-            //console.log(array);
-    });  
+    fs.readFile('localcoef.temporary.txt', function (err, data) {
+        if (err) throw err;
+
+        data1 = data.toString()
+        arr = data1.split(',').map(Number)
+        var array = []
+        array = [arr]
+        myMethod(array)
+        //console.log(array);
+    });
     //console.log(array)
 
-    function myMethod(array){
-        fs.writeFile('localcoef.temporary.txt', '', function(){console.log('done')})
-        services.get_best_model(req,res,array) 
+    function myMethod(array) {
+        fs.writeFile('localcoef.temporary.txt', '', function () { console.log('done') })
+        services.get_best_model(req, res, array)
     }
 
-    //var array=new Array()
-    // function myMethod(){
-    //     fs.readFile('localcoef.temporary.txt',function(err,data){
-    //         if(err) throw err;
-    //         data1=data.toString()
-    //         arr=data1.split(',').map(Number)
-    //         var array=[]
-    //         array=[arr]
-    //         //console.log(array)
-    //     })
-    //     return array
-    // }
-    // var array=myMethod()
-    // console.log(array);
-    //fs.writeFile('localcoef.temporary.txt', '', function(){console.log('done')})
-    //services.get_best_model(req,res) 
-      
 })
 
 
-app.get('/aggregate', async (req, res ) =>{
+app.get('/aggregate', async (req, res) => {
     services.getDoc()
     services.get_fileDownload()
 });
